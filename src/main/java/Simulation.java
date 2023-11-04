a363aa9ecb1d3f0086bec5e97b9a165f705b555d
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-04 18:18:12 -0400

    simulation in progress

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,11 @@
 public class Simulation {
+    int numberOfDies;
+    int numberOfThrows;
 
+    public Simulation(int numberOfDies, int numberOfThrows){
+        this.numberOfDies = numberOfDies;
+        this.numberOfThrows = numberOfThrows;
 
+    }
 
 }
--- src/main/test/SimulationTests.java
+++ src/main/test/SimulationTests.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SimulationTests {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimulationTests {
+
+    @Test
+    public void testSimulationConstructor(){
+        int numberOfDies = 2;
+        int numberOfTosses = 3;
+
+        Simulation simulation = new Simulation(numberOfDies, numberOfTosses);
+
+        Assert.assertNotNull(simulation);
+    }
+
+    @Test
+    public void testRunSimulation(){
+        int numberOfDies = 2;
+        int numberOfTosses = 3;
+        Simulation simulation = new Simulation(numberOfDies, numberOfTosses);
+
+
+    }
 }
